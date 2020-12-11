c3a36fa7d4ce4aab832f991b7e94e241399e214a
chuyong <chuyong@3k.com>
2020-12-11 17:17:43 +0800

    init

---
+++ name.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Description :
+
+
+def say_hello(name=None, age=None):
+    """ 测试方法 """
+
+    name = name
+
+    age = age
+
+    branch = "master"
+
+    desc = "this is master branch content. so let me see."
+
